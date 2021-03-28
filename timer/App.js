ac1af16518932e81b3b9da082a8d5da79e4dbb13
Joshmerm <joshmerm@gmail.com>
2021-03-28 12:50:57 -0700

    Update, works properly, styling

--- timer/App.js
+++ timer/App.js
@@ -1,20 +1,20 @@
 
 import React, {useEffect, useState} from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, TouchableWithoutFeedback, Keyboard, Vibration } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, TouchableOpacity, TextInput, TouchableWithoutFeedback, Keyboard, Vibration, SafeAreaView } from 'react-native';
 // import {vibrate} from './utils';
 
 
 export default function App() {
 
-  const [workTimerSeconds, setWorkTimerSeconds] = useState("03");
-  const [workTimerMinutes, setWorkTimerMinutes] = useState("00");
-  const [breakTimerSeconds, setBreakTimerSeconds] = useState("05");
-  const [breakTimerMintues, setBreakTimerMinutes] = useState("00");
+  const [workTimerSeconds, setWorkTimerSeconds] = useState("00");
+  const [workTimerMinutes, setWorkTimerMinutes] = useState("25");
+  const [breakTimerSeconds, setBreakTimerSeconds] = useState("00");
+  const [breakTimerMintues, setBreakTimerMinutes] = useState("25");
 
-  const [workTimerSecondsHolder, setWorkTimerSecondsHolder] = useState("03");
-  const [workTimerMinutesHolder, setWorkTimerMinutesHolder] = useState("00");
-  const [breakTimerSecondsHolder, setBreakTimerSecondsHolder] = useState("05");
-  const [breakTimerMintuesHolder, setBreakTimerMinutesHolder] = useState("00");
+  const [workTimerSecondsHolder, setWorkTimerSecondsHolder] = useState(workTimerSeconds);
+  const [workTimerMinutesHolder, setWorkTimerMinutesHolder] = useState(workTimerMinutes);
+  const [breakTimerSecondsHolder, setBreakTimerSecondsHolder] = useState(breakTimerSeconds);
+  const [breakTimerMintuesHolder, setBreakTimerMinutesHolder] = useState(breakTimerMintues);
 
   const [isWork, setIsWork] = useState(true); 
   const [stop, setStop] = useState(false);
@@ -62,7 +62,6 @@ export default function App() {
   });
 
   const reduce = () => {
-    console.log(workTimerSeconds);
     if(isWork && !stop){
       if(workTimerSeconds == "00" && workTimerMinutes == "00"){
         // setWorkTimerMinutes(workTimerMinutes)
@@ -70,7 +69,7 @@ export default function App() {
         setBreakTimerMinutes(breakTimerMintuesHolder);
         setBreakTimerSeconds(breakTimerSecondsHolder);
         Vibration.vibrate([0, 500, 100, 200]);
-        // Vibration.cancel()
+        Vibration.cancel()
       }else if(workTimerSeconds == "00"){
         //go to break;
         let min = parseInt(workTimerMinutes) - 1;
@@ -83,15 +82,17 @@ export default function App() {
         setWorkTimerSeconds(seconds);
       }
     }else if(!stop){
-      if(breakTimerSeconds == "00" && breakTimerSeconds == "00"){
+      if(breakTimerSeconds == "00" && breakTimerMintues == "00"){
         // setWorkTimerMinutes(workTimerMinutes)
+        console.log("Here")
         setIsWork(true);
         setWorkTimerMinutes(workTimerMinutesHolder);
         setWorkTimerSeconds(workTimerSecondsHolder);
         Vibration.vibrate([0, 500, 100, 200]);
-        // Vibration.cancel()
+        Vibration.cancel()
 
       }else if(breakTimerSeconds == "00"){
+        console.log("Here")
         //go to break;
         let min = parseInt(breakTimerMintues) - 1;
         if(min < 10) min = "0" + min;
@@ -113,50 +114,78 @@ export default function App() {
 
 
   return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+    
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false} style={styles.container}>
+      
       <View style={styles.container}>
-      {isWork ? 
-        <Text>
-          Work Timer
+        <Text style={styles.timerType}>
+          {isWork ? 
+            "Work Timer"
+            :
+            "Break Timer"
+          }
         </Text>
-      :
-        <Text>
-          Break Timer
+        <Text style={styles.timer}>
+          {isWork ? 
+            workTimerMinutes + ":" + workTimerSeconds
+          :
+            breakTimerMintues + ":" + breakTimerSeconds
+          }
         </Text>
-      }
-      {isWork ? 
-        <Text>
-        {workTimerMinutes + ":" + workTimerSeconds}
-      </Text>
+      <View style={{flexDirection: 'row', margin: 10}}>
+      {!stop ? 
+        <TouchableOpacity onPress={() => setStop(!stop)} style={[styles.button, styles.stop]}>
+          <Text>Stop</Text>
+        </TouchableOpacity>
       :
-      <Text>
-      {breakTimerMintues + ":" + breakTimerSeconds}
-    </Text>
-      }
-      <TouchableOpacity onPress={() => setStop(!stop)} style={styles.button}>
-        {!stop ? <Text>Stop</Text> : <Text>Start</Text>}
+      <TouchableOpacity onPress={() => setStop(!stop)} style={[styles.button, styles.start]}>
+        <Text>Start</Text>
       </TouchableOpacity>
+    
+      }
+      
       <TouchableOpacity onPress={reset} style={styles.button}>
         <Text>
           Reset
         </Text>
       </TouchableOpacity>
-      <Text>Worker Time: </Text>
-      <View style={styles.row}>
-        <Text style={{alignSelf: 'center'}}>Mins: </Text>
-        <TextInput style={styles.in} maxLength={2} onChangeText={text => setWorkMin(text)} value={workTimerMinutesHolder}/>
-        <Text style={{alignSelf: 'center'}}>Secs: </Text>
-        <TextInput style={styles.in}  maxLength={2} onChangeText={text => setWorkSec(text)} value={workTimerSecondsHolder}/>
+
       </View>
-      <Text>Break Time: </Text>
-      <View style={styles.row}>
-        <Text style={{alignSelf: 'center'}}>Mins: </Text>
-        <TextInput style={styles.in} maxLength={2} onChangeText={text => setBreakMin(text)} value={breakTimerMintuesHolder}/>
-        <Text style={{alignSelf: 'center'}}>Secs: </Text>
+      <Text style={styles.label}>Worker Time</Text>
+
+      <View style={styles.line}/>
+
+      <View style={styles.input}>
+        <Text style={{fontSize: 20, fontWeight: 'bold'}}>Minutes</Text>
+        <TextInput style={styles.in} maxLength={2} onChangeText={text => setWorkMin(text)} value={workTimerMinutesHolder}/>
+      </View>
+
+      <View style={styles.input}>
+        <Text style={{fontSize: 20, fontWeight: 'bold'}}>Seconds</Text>
+        <TextInput style={styles.in}  maxLength={2} onChangeText={text => setWorkSec(text)} value={workTimerSecondsHolder}/>        
+      </View>
+
+      <Text style={styles.label}>Break Time</Text>
+
+      <View style={styles.line}/>
+
+      <View style={styles.input}>
+        <Text style={{fontSize: 20, fontWeight: 'bold'}}>Minutes</Text>
+        <TextInput keyboardType={'number-pad'} style={styles.in} maxLength={2} onChangeText={text => setBreakMin(text)} value={breakTimerMintuesHolder}/>
+      </View>
+
+      <View style={styles.input}>
+        <Text style={{fontSize: 20, fontWeight: 'bold'}}>Seconds</Text>
         <TextInput style={styles.in}  maxLength={2} onChangeText={text => setBreakSec(text)} value={breakTimerSecondsHolder}/>
       </View>
+
       </View>
     </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -169,20 +198,64 @@ const styles = StyleSheet.create({
     height: '100%',
     width: '100%'
   },
+  input: {
+    width: '60%',
+    // margin: 10
+    // padding: 10
+    // backgroundColor: 'green'
+
+  },
   button: {
-    backgroundColor: 'blue',
-    width: 150,
-    height: 50,
+    width: 100,
+    height: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 100/2,
+    borderWidth: 4,
+    shadowColor: 'grey',
+    shadowOpacity: 10,
+    elevation: 5,
+    shadowOffset : { width: 1, height: 4},
+    margin: 10
   },
   in: {
-    width: '20%',
+    width: '100%',
     height: 50,
-    margin: 10,
-    backgroundColor: 'yellow',
+    borderColor: '#479FE7',
+    borderWidth: 2,
+    borderRadius: 17,
+    padding: 15,
+    // margin: 10,
+    // backgroundColor: 'yellow',
   },
   row: {
     justifyContent: 'center',
     width: '100%',
     flexDirection: 'row'
+  },
+  timerType: {
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  timer: {
+    fontSize: 40,
+  },
+  start: {
+    borderColor: 'green',
+  },
+  stop: {
+    borderColor: 'red',
+  },
+  label: {
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+  line: {
+    width: '60%',
+    // width: 100,
+    height: 2,
+    margin: 5,
+    backgroundColor: '#479FE7',
+    borderRadius: 17,
   }
 });
